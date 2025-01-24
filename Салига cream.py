ed554081c4d4c101dd0fe55e574e48912a956343
Sasha <aleks.aleks010409@gmail.com>
2025-01-24 13:37:50 +0200

    Add files via upload

---
+++ Салига cream.py
@@ -0,0 +1,8 @@
+
+def introduce_myself(name, college, hobby):
+    print(f"Привіт! Я {name}, навчаюсь у коледжі {college}. Дуже люблю {hobby}. Всім привіт!")
+
+# Вызов функции
+introduce_myself("Саша Салига", "КТГГ", "тенніс")
+
+
